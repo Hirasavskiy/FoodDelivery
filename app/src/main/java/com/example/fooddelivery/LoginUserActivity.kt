2adab53cc3a33c8c1017a57aafd5652aa914d131
Hirasavskiy <90036297+Hirasavskiy@users.noreply.github.com>
2024-04-07 21:17:02 +0300

    some changes

--- app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
+++ app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
@@ -1,11 +1,20 @@
 package com.example.fooddelivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fooddelivery.databinding.ActivityLoginUserBinding
 
+private lateinit var binding: ActivityLoginUserBinding
 class LoginUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_user)
+        binding = ActivityLoginUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goSingUpUser.setOnClickListener(){
+            val intent = Intent(this@LoginUserActivity, SignUpUserActivity ::class.java)
+            startActivity(intent)
+        }
     }
 }
